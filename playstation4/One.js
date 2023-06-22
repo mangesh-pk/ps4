3aa606e634f7684cead36fade5011601f3efa0b0
mangesh-pk <135018801+mangesh-pk@users.noreply.github.com>
2023-06-22 14:25:24 +0530

    Add files via upload

---
+++ playstation4/One.js
@@ -0,0 +1,25 @@
+
+
+// var marks = Array (99,19,29,49,29,69,39,33,29,99,69,49);
+// console.log(marks.indexof(19));
+// console.log(marks.indexof(999));
+// console.log(marks.indexof(29));
+// console.log(marks.indexof(29,3));
+// console.log(marks.indexof(29,marks.indexof(29)+1));
+
+
+
+// var marks = Array (99,19,29,49,29,69,39,33,29,99,69,49);
+// console.log(marks.lastindexof(29));
+// console.log(marks.lastindexof(999));
+// console.log(marks.lastindexof(19));
+// console.log(marks.lastindexof(29,8));
+
+
+
+// var marks = Array(9,1,29,4,29,69,0,33,29,99,6,49);
+// var products = Array('java','python','angular','react','go','c','c++','c#');
+// console.log(products);
+// products.sort();
+// console.log(products);
+// products.sort();
